ae9953e14b01a5b5a5a0cdaff989bfc02e8c706a
John Loverich <john@glodonusa.com>
2025-03-29 17:35:42 -0700

    Fixing bug

--- non_uniform_piecewise_layers/__init__.py
+++ non_uniform_piecewise_layers/__init__.py
@@ -2,6 +2,6 @@ from .piecewise_linear import NonUniformPiecewiseLinear
 from .adaptive_piecewise_linear import AdaptivePiecewiseLinear
 from .adaptive_piecewise_mlp import AdaptivePiecewiseMLP
 from .adaptive_piecewise_conv import AdaptivePiecewiseConv2d
-
+from .efficient_adaptive_piecewise_conv import EfficientAdaptivePiecewiseConv2d
 
 __all__ = ['NonUniformPiecewiseLinear', 'AdaptivePiecewiseLinear', 'AdaptivePiecewiseMLP', 'AdaptivePiecewiseConv2d','EfficientAdaptivePiecewiseConv2d']
